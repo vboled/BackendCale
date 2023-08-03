9e1d8c4030cb807c0ee3dfc44e6eb1412290a447
vboled <vboled@gmail.com>
2023-08-03 15:45:29 +0300

    Readme updated

--- src/main/java/com/vboled/cale/controllers/TestController.java
+++ src/main/java/com/vboled/cale/controllers/TestController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/messages")
-    public String getMessage() {
-        return "Hello from Docker!";
-    }
-
     @GetMapping("/hello")
     public String getHello() {
         return "Hello from Backend!";

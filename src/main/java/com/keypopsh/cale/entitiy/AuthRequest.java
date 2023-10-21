6d938b533d2a6c11f614f95f77c0bb0f9ebe0553
vboled <vboled@gmail.com>
2023-10-21 21:14:22 +0300

    Some changes

---
+++ src/main/java/com/keypopsh/cale/entitiy/AuthRequest.java
@@ -0,0 +1,15 @@
+package com.keypopsh.cale.entitiy;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
+}
